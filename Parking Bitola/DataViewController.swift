11884a5663a89de2f8cf4ba7063642d06a6c481e
Aleksandar Ruzin <ruzin.aleksandar@gmail.com>
2016-09-07 19:48:07 +0200

    Initial Commit

---
+++ Parking Bitola/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Parking Bitola
+//
+//  Created by Aleksandar Ruzin on 9/7/16.
+//  Copyright © 2016 Aleksandar Ruzin. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ Parking BitolaTests/Parking_BitolaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Parking_BitolaTests.swift
+//  Parking BitolaTests
+//
+//  Created by Aleksandar Ruzin on 9/7/16.
+//  Copyright © 2016 Aleksandar Ruzin. All rights reserved.
+//
+
+import XCTest
+@testable import Parking_Bitola
+
+class Parking_BitolaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
